4b87e522a8c867a96f8ba442b1cecdc4f48eafb8
noeliechristycatolin <noeliechristycatolin@gmail.com>
2021-11-26 01:13:00 +0800

    bottomsheet visibility

--- lib/features/search_station/search_station_map_screen.dart
+++ lib/features/search_station/search_station_map_screen.dart
@@ -28,6 +28,7 @@ class _SearchStationMapScreenState extends State<SearchStationMapScreen> {
 
   int groupValue = -1;
   bool hasStationSelected = false;
+  bool isBottomSheetDisplayed = true;
   Station? selectedStation;
 
   @override
@@ -69,69 +70,73 @@ class _SearchStationMapScreenState extends State<SearchStationMapScreen> {
                 onMapCreated: (GoogleMapController controller) async {
                   _controller = controller;
                   _currentPosition = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-                  updateMapCamera(_currentPosition.latitude, _currentPosition.longitude);
-                },))
+                  _updateMapCamera(_currentPosition.latitude, _currentPosition.longitude);
+                },)),
+
             ],
           ),
         ),
-        bottomSheet: Container(
-          decoration: const BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(topLeft: Radius.circular(30), topRight: Radius.circular(30))
-          ),
-          height: 250,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children:  [
-                      hasStationSelected ?
-                        InkWell(
-                            child:  const Text('Back to list',
-                              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700, color: Colors.lightBlue),),
-                              onTap: () => resetSelection(),
-                        )
-                        : const Text('Nearby Stations', style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700),),
-                         InkWell(
-                             child: Text('Done',
-                                 style: TextStyle(fontSize: 18, fontWeight: FontWeight.w800, color: hasStationSelected ? Colors.lightBlue : Colors.grey)),
-                                onTap: hasStationSelected ? () => Navigator.pop(context) : (){},
-                         ),
-                    ],),
-                ),
-                const SizedBox(height: 10,),
-                hasStationSelected ?
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children:  [
-                          Text(selectedStation!.name, style: const TextStyle(fontWeight: FontWeight.w600), overflow: TextOverflow.ellipsis,),
-                          Text(selectedStation!.address, ),
-                          Row(children: [Text(selectedStation!.city,),Text(selectedStation!.area,)],),
-                          const SizedBox(height: 30,),
-                          Row(children: [
-                            IconLabel(icon: const Icon(Icons.car_repair), label: '${convertDistance(selectedStation!)} km away'),
-                            const SizedBox(width: 20,),
-                            const IconLabel(icon: Icon(Icons.access_time), label: 'Open 24 hrs'),
-                          ],)
-                        ],
-                      ),
-                    )
-                    : Expanded(
-                      child: ListView.builder(
-                        itemCount: state.stations.length,
-                          itemBuilder: (context, index) => StationSelection(
-                            index: index,
-                            station: state.stations[index],
-                            groupValue: groupValue,
-                            onChanged: (value) =>_handleRadioValueChange(value, state.stations[index])))
-                    )
-              ],
+        bottomSheet: Visibility(
+          visible: isBottomSheetDisplayed,
+          child: Container(
+            decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(topLeft: Radius.circular(30), topRight: Radius.circular(30))
+            ),
+            height: 250,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children:  [
+                        hasStationSelected ?
+                          InkWell(
+                              child:  const Text('Back to list',
+                                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700, color: Colors.lightBlue),),
+                                onTap: () => _resetSelection(),
+                          )
+                          : const Text('Nearby Stations', style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700),),
+                           InkWell(
+                               child: Text('Done',
+                                   style: TextStyle(fontSize: 18, fontWeight: FontWeight.w800, color: hasStationSelected ? Colors.lightBlue : Colors.grey)),
+                                  onTap: hasStationSelected ? () => _onDismissedBottomSheet()  : (){},
+                           ),
+                      ],),
+                  ),
+                  const SizedBox(height: 10,),
+                  hasStationSelected ?
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children:  [
+                            Text(selectedStation!.name, style: const TextStyle(fontWeight: FontWeight.w600), overflow: TextOverflow.ellipsis,),
+                            Text(selectedStation!.address, ),
+                            Row(children: [Text(selectedStation!.city,),Text(selectedStation!.area,)],),
+                            const SizedBox(height: 30,),
+                            Row(children: [
+                              IconLabel(icon: const Icon(Icons.car_repair), label: '${convertDistance(selectedStation!)} km away'),
+                              const SizedBox(width: 20,),
+                              const IconLabel(icon: Icon(Icons.access_time), label: 'Open 24 hrs'),
+                            ],)
+                          ],
+                        ),
+                      )
+                      : Expanded(
+                        child: ListView.builder(
+                          itemCount: state.stations.length,
+                            itemBuilder: (context, index) => StationSelection(
+                              index: index,
+                              station: state.stations[index],
+                              groupValue: groupValue,
+                              onChanged: (value) =>_handleRadioValueChange(value, state.stations[index])))
+                      )
+                ],
+              ),
             ),
           ),
         ),
@@ -139,11 +144,19 @@ class _SearchStationMapScreenState extends State<SearchStationMapScreen> {
     );
   }
 
-  void _navigateToSearchStationListScreen(BuildContext context) async {
+  _onDismissedBottomSheet(){
+    print('_onDismissedBottomSheet');
+    setState(() {
+      isBottomSheetDisplayed = false;
+    });
+  }
+
+  _navigateToSearchStationListScreen(BuildContext context) async {
     final List<Object> result = await Navigator.push(context, MaterialPageRoute(builder: (context)=> const SearchStationListScreen()));
     Station selectedStation = result[0] as Station;
     int selectedIndex = result[1] as int;
     _handleRadioValueChange(selectedIndex, selectedStation);
+    isBottomSheetDisplayed = true;
   }
 
   _handleRadioValueChange(int value, Station station) {
@@ -154,18 +167,18 @@ class _SearchStationMapScreenState extends State<SearchStationMapScreen> {
 
       double lat = double.parse(station.lat);
       double lng = double.parse(station.lng);
-      updateMapCamera(lat, lng);
+      _updateMapCamera(lat, lng);
     });
   }
 
-  updateMapCamera(double lat, double lng){
+  _updateMapCamera(double lat, double lng){
     _cameraPosition = CameraPosition(target: LatLng(lat, lng), zoom: 20,);
     _controller.animateCamera(
       CameraUpdate.newCameraPosition(_cameraPosition),
     );
   }
 
-  resetSelection(){
+  _resetSelection(){
     print('resetSelection');
     setState(() {
       // groupValue = -1;
